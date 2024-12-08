569ef1915767da1476d23c8dce4368c67ea837ea
Nishan <nishannarban001@gmail.com>
2024-12-08 20:05:33 +0530

    commit

--- src/components/appbar.tsx
+++ src/components/appbar.tsx
@@ -1,8 +1,25 @@
 export default function AppBar() {
   return (
     <div className="flex flex-col border ">
-      <div className="flex border">ROW-1</div>
-      <div className="flex border">ROW-2</div>
+      <div className="flex border">ROW-1
+        <div>fgfg
+          <a href="https://eksisozluk.com/"><img src="D:\AIM\CODE\Projects-Collaboration\React-reddit\neonlights\public\next.svg" alt="tiuuiyyfio" /></a>
+        </div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <div className="flex border justify-between">
+        <div>gündem</div>
+        <div>debe</div>
+        <div>sorunsallar</div>
+        <div>#spor</div>
+        <div>#ilişkiler</div>
+        <div>#yaşam</div>
+        <div>...</div>
+        <div></div>
+
+      </div>
     </div>
   );
 }
