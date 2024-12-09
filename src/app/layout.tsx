73a14d2116cfcaf8e9ec8e56ce645e7c4cfc976e
yavuz dogan <y.selimdogan@gmail.com>
2024-12-09 21:44:41 +0300

    search bar added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import AppBar from "@/components/appbar";
+import { Container } from "@mui/material";
 
 export const metadata: Metadata = {
   title: "ChitChat App",
@@ -15,14 +16,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="flex flex-col justify-between">
-        <AppBar />
+        <Container maxWidth="xl" className="flex flex-col justify-between h-full">
+          <AppBar />
 
-        <div className="flex border flex-1">
-          <div className="border">Sidebar</div>
-          {children}
-        </div>
-
-        <div className="border">FOOTER</div>
+          <div className="flex border flex-1">
+            <div className="border">Sidebar</div>
+            {children}
+          </div>
+          <div className="border">FOOTER</div>
+        </Container>
       </body>
     </html>
   );
--- src/components/appbar.tsx
+++ src/components/appbar.tsx
@@ -1,13 +1,19 @@
+import Image from "next/image";
+import Link from "next/link";
+import SearchBar from "./searchBar";
+
 export default function AppBar() {
   return (
     <div className="flex flex-col border ">
-      <div className="flex border">ROW-1
-        <div>fgfg
-          <a href="https://eksisozluk.com/"><img src="D:\AIM\CODE\Projects-Collaboration\React-reddit\neonlights\public\next.svg" alt="tiuuiyyfio" /></a>
-        </div>
-        <div></div>
-        <div></div>
-        <div></div>
+      <div className="flex border gap-3 pt-4 pb-4">
+        <Link href={"/"} className="mr-10">
+          <Image src="/logo.png" alt="logo of the app" width={40} height={40} />
+        </Link>
+
+        <SearchBar />
+        <div className="flex-1"></div>
+        <div className="">Sign In</div>
+        <div className="">Register</div>
       </div>
       <div className="flex border justify-between">
         <div>gündem</div>
@@ -18,7 +24,6 @@ export default function AppBar() {
         <div>#yaşam</div>
         <div>...</div>
         <div></div>
-
       </div>
     </div>
   );
